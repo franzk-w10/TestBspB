fb66b9e8b14def15f6cad1803b9c46197b3afb54
kranzfra <kranzfra@EDV3-01.htlw10.at>
2015-06-02 13:59:01 +0200

    Erweiterung 1

--- Prog_B/Prog_B.java
+++ Prog_B/Prog_B.java
@@ -3,10 +3,29 @@ public class Prog_B {
 
 	public static void main(String[] args) {
 
+		titel("Fibonacci-Folge");
+		
 		ende();
 
 	}
 
+	
+	static void titel(String text) {
+		
+		System.out.print("\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			System.out.print("*");
+		
+		System.out.print("\n\t\t*  "+ text + "  *\n\t\t");
+		
+		for (int i=0; i<text.length()+6; i++)
+			System.out.print("*");
+		
+		System.out.println("\n");
+	}	
+	
+	
 	static void ende() {
 		
 		System.out.println("\n\n\t das wars!");
